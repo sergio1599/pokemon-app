fbf964e65b8494bf22ea454e133257ca9c7cc384
sergio1599 <sergioq15@hotmail.com>
2022-08-15 10:11:25 -0500

    load pokemons with axios and get static props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,31 @@
-import { Button } from "@nextui-org/react";
-import { NextPage } from "next";
+import { NextPage, GetStaticProps } from "next";
 import { Layout } from "../components/layouts";
+import { pokeApi } from "../api";
+import { PokemonListResponse } from "../interfaces";
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+  console.log({ props });
+
   return (
     <Layout title="Listado de Pokemons">
-      <Button color="gradient">Hola mundo</Button>
+      <ul>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+      </ul>
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await pokeApi.get<PokemonListResponse>("/pokemon?limit=151");
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  };
 };
 
 export default HomePage;

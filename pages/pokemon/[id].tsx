1a1468be32b02760142507d90068920ef8b98a86
sergio1599 <sergioq15@hotmail.com>
2022-08-22 18:53:22 -0500

    adding styles to [id].tsx

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -2,16 +2,76 @@ import { Layout } from "../../components/layouts";
 import { NextPage, GetStaticProps } from "next";
 import { GetStaticPaths } from "next";
 import { pokeApi } from "../../api";
-import { Pokemon } from "../../interfaces/";
+import { Button, Card, Container, Grid, Image, Text } from "@nextui-org/react";
+import { Pokemon } from "../../interfaces";
 
 interface Props {
-  pokemon: any;
+  pokemon: Pokemon;
 }
 
 export const PokemonPage: NextPage<Props> = ({ pokemon }) => {
   return (
     <Layout title="Algun pokemon">
-      <h1>{pokemon.name}</h1>
+      <Grid.Container css={{ marginTop: "5px" }} gap={2}>
+        <Grid xs={12} sm={4}>
+          <Card hoverable css={{ padding: "30px" }}>
+            <Card.Body>
+              <Card.Image
+                src={
+                  pokemon.sprites.other?.dream_world.front_default ||
+                  "/no-image.png"
+                }
+                alt={pokemon.name}
+                width="100%"
+                height={200}
+              />
+            </Card.Body>
+          </Card>
+        </Grid>
+        <Grid xs={12} sm={8}>
+          <Card>
+            <Card.Header
+              css={{ display: "flex", justifyContent: "space-between" }}
+            >
+              <Text h1 transform="capitalize">
+                {pokemon.name}
+              </Text>
+              <Button color="gradient" ghost>
+                Guardar en favoritos
+              </Button>
+            </Card.Header>
+            <Card.Body>
+              <Text size={30}>Sprites:</Text>
+              <Container direction="row" display="flex" gap={0}>
+                <Image
+                  src={pokemon.sprites.front_default}
+                  alt={pokeApi.name}
+                  width={100}
+                  height={100}
+                />
+                <Image
+                  src={pokemon.sprites.back_default}
+                  alt={pokeApi.name}
+                  width={100}
+                  height={100}
+                />
+                <Image
+                  src={pokemon.sprites.front_shiny}
+                  alt={pokeApi.name}
+                  width={100}
+                  height={100}
+                />
+                <Image
+                  src={pokemon.sprites.back_shiny}
+                  alt={pokeApi.name}
+                  width={100}
+                  height={100}
+                />
+              </Container>
+            </Card.Body>
+          </Card>
+        </Grid>
+      </Grid.Container>
     </Layout>
   );
 };

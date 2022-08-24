eae0d05b5ab9b0ace219b4a348d1d2219007959e
sergio1599 <sergioq15@hotmail.com>
2022-08-24 14:05:07 -0500

    use Next useRouter for navigate to the pokemon page from the favorites page

--- components/pokemon/FavoriteCardPokemon.tsx
+++ components/pokemon/FavoriteCardPokemon.tsx
@@ -1,13 +1,21 @@
 import React, { FC } from "react";
 import { Grid, Card } from "@nextui-org/react";
+import { useRouter } from 'next/router';
 
 interface Props {
   pokemonId: number;
 }
 
 export const FavoriteCardPokemon: FC<Props> = ({ pokemonId }) => {
+
+  const router = useRouter(); 
+
+  const onFavoriteClicked = () =>{
+    router.push(`/pokemon/${pokemonId}`)
+  }
+
   return (
-    <Grid xs={6} sm={3} md={2} xl={1} key={pokemonId}>
+    <Grid xs={6} sm={3} md={2} xl={1} key={pokemonId} onClick={onFavoriteClicked}>
       <Card hoverable clickable css={{ padding: 10 }}>
         <Card.Image
           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg`}
